23e5b5eea4cccff6edf70d6a170372267c0a1e95
info@christoph-schober.com <info@christoph-schober.com>
2020-11-28 15:36:18 +0100

    init

---
+++ src/components/RangeSlider.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-field label="Lieferinterval Range">
+    <b-slider v-model="endpoints" :max="200" :min="10" :step="1" ticks @input="generateRangeArray">
+    </b-slider>
+  </b-field>
+</template>
+
+<script>
+export default {
+  name: "RangeSlider",
+
+  data(){
+    return {
+      endpoints: [],
+      range: []
+    }
+  }
+
+  methods: {
+    generateRangeArray(range) {
+      console.log(range);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
